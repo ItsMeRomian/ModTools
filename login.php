31ab1f4c9405b3bbaea69abdef2014b12bd255fe
Romian Tairovski <romian.tairovski@gmail.com>
2021-11-24 00:21:05 +0100

    trying something

--- login.php
+++ login.php
@@ -11,6 +11,36 @@ $rank = "SELECT name FROM ranks WHERE id = '" . $row['rank'] . "'";
 
 $rankresult = $conn->query($rank);
 $rankrow = $rankresult->fetch_array();
+
+$id = $row['id'];
+
+//Haal alle logs op
+$logs = $conn->query("SELECT * FROM `logs` WHERE user_id = $id AND log_type = 'failedlogin'")->fetch_all(MYSQLI_ASSOC);
+
+//de eerste log van max 1 uur geleden
+$maxBack = strtotime("now - 1 hour");
+$start = getClosest($maxBack, $logs);
+
+//De laatste log
+$end = strtotime(end($logs)['timestamp']);
+
+
+
+function getClosest($search, $logs) {
+	$end = [];
+	foreach($logs as $log) {
+		array_push($end, strtotime($log['timestamp']));
+	}
+
+	$closest = null;
+	foreach ($end as $item) {
+	   if ($closest === null || abs($search - $closest) > abs($item - $search)) {
+		  $closest = $item;
+	   }
+	}
+	return $closest;
+ }
+
 if (password_verify($_POST["password"], $row["password"])) {
 	if ($row['rank'] >= 6) {
 		createLog('login', $row['id'], null);
@@ -27,4 +57,7 @@ if (password_verify($_POST["password"], $row["password"])) {
 			die("Je hebt rank <b>" . $row['rank'] . "</b>. Je moet minimaal rank <b>6</b> hebben om in te loggen.");
 		}
 	}
+} else {
+	createLog('failedLogin', $row['id'], null, "Wrong pass");
+	die("Wrong password");
 }
