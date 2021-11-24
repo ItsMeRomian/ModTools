8db6f5659f9c09e4ad878deb76ec3a13e0918dfb
Romian Tairovski <romian.tairovski@gmail.com>
2021-11-24 10:47:58 +0100

    login rate limit

--- login.php
+++ login.php
@@ -14,33 +14,26 @@ $rankrow = $rankresult->fetch_array();
 
 $id = $row['id'];
 
-//Haal alle logs op
+// Haal alle logs op
 $logs = $conn->query("SELECT * FROM `logs` WHERE user_id = $id AND log_type = 'failedlogin'")->fetch_all(MYSQLI_ASSOC);
+if (!$logs) throw new Error("No logs found");
 
-//de eerste log van max 1 uur geleden
-$maxBack = strtotime("now - 1 hour");
-$start = getClosest($maxBack, $logs);
+//Set vars
+$hourBack = strtotime('now - 1 hour');
+$firstInHourBack = null;
+$end = null;
 
-//De laatste log
-$end = strtotime(end($logs)['timestamp']);
-
-
-
-function getClosest($search, $logs) {
-	$end = [];
-	foreach($logs as $log) {
-		array_push($end, strtotime($log['timestamp']));
+//Loop over elke verkeerde inlog. Als een verkeerde inlog in het vorige uur valt, setten we de 1e poging als $fisrtInHourBack.
+// We pakken ook de allerlaatste poging. Als de hoeveelheid van deze pogingen > is dan 5. Dus 5x in het afgelopen uur, dan blokeren we de volgende inlogpoging.
+for ($i = 0; $i < count($logs); $i++) {
+	if (strtotime($logs[$i]['timestamp']) > $hourBack) {
+		$firstInHourBack = $i;
 	}
-
-	$closest = null;
-	foreach ($end as $item) {
-	   if ($closest === null || abs($search - $closest) > abs($item - $search)) {
-		  $closest = $item;
-	   }
-	}
-	return $closest;
- }
-
+	$end = $i;
+}
+if (($end - $firstInHourBack) >= 5) {
+	die("Je hebt te vaak verkeerd ingelogd in 1 uur (5x). Probeer het later nog eens.");
+}
 if (password_verify($_POST["password"], $row["password"])) {
 	if ($row['rank'] >= 6) {
 		createLog('login', $row['id'], null);
